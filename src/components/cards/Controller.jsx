a647b1cfe1a00ba15c67c2a256f1ccd8131dd7d9
simo-ce <ait.ouahamne.01@gmail.com>
2023-03-03 20:33:00 +0100

    tooltip

--- src/components/cards/Controller.jsx
+++ src/components/cards/Controller.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
 import Toggle from '../../components/Switch';
 import info from '../../assets/info.svg'
+import 'react-tooltip/dist/react-tooltip.css'
+import { Tooltip } from 'react-tooltip'
 
 const Controller = ({ icon, name, count, type }) => {
     const [webcam, setWebcam] = useState(false);
     const handleSwitch = (checked) => {
         setWebcam(checked);
     }
+    const tooltipContent = 'When the countdown is finished, the system will automatically move to the next question.'
     return (
         <div className='bg-white rounded flex flex-col justify-center items-center p-3 gap-3 w-full'>
-            <img src={info} alt="info" className='self-end' />
-            <img src={icon} alt="" />
+            <Tooltip id="info-tooltip" className='bg-white text-black rounded-md w-64 text-start shadow-sm' />
+            <img src={info} alt="info" className='self-end cursor-pointer' data-tooltip-id="info-tooltip" data-tooltip-content={tooltipContent} />
+            <img src={icon} alt={name} />
             <span className='font-medium whitespace-nowrap'>{name}</span>
             {type === 'count' && <div className='flex flex-row justify-center items-stretch gap-1'>
                 <div className='rounded flex justify-center items-center bg-gray-350 text-gray-650'>
