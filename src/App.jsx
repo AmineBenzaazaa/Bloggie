d93c9501b01d866c8803d0ecd6d8e77a8df0a7e0
simo-ce <ait.ouahamne.01@gmail.com>
2023-03-04 12:27:39 +0100

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -157,15 +157,6 @@ function App() {
         {/* End training quiz */}
 
         {/* Controllers */}
-        {/* <div className='mb-5 flex flex-col flex-wrap lg:flex-nowrap sm:flex-row gap-3 items-stretch relative'>
-          {controllers.map((controller, idx) => <Controller key={idx} {...controller} />)}
-          <div className='bg-white text-gray-450 h-fit p-1 rounded-md absolute -left-10 top-1/2 -translate-y-1/2'>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-              <path fillRule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clipRule="evenodd" />
-            </svg>
-          </div>
-        </div> */}
-
         <div className='relative w-full mb-10'>
           <div className="scroll-container" ref={containerRef}>
             <div className='flex flex-row items-stretch gap-3 w-full'>
